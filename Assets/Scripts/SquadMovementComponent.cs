e97c5ebf998c504c3b51b1475cbf5b9655926e7b
Javier <javcomas@ucm.es>
2021-10-01 12:39:40 +0200

    COSAS

---
+++ Assets/Scripts/SquadMovementComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadMovementComponent : MonoBehaviour
+{
+    private int _initialDirection;
+    private float _verticalSpeed=0.001f;
+    private float _horizontalSpeed=1.5f;
+    private float _frequency=1.5f;
+    public float _amplitude=1.5f;//diametro del movimiento
+     float y;//para comprobar la altura establecida para destruir el objeto
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //el volar x esta entre -1 y 1 y se actualiza de manera constante del el tiempo real,
+        //si le multiplicamos por frequency aceleramoso frenamos y la amplitud del movimiento
+        float x = Mathf.Cos(Time.time * _frequency) * _amplitude;
+
+         y = transform.position.y;
+
+        //el transform.position es par que se vea con el fondo este delante
+        transform.position = new Vector3(x,y-_verticalSpeed,0);
+    }
+    public float Altura() { return y; }
+}
---
+++ Assets/Scripts/WorldVerticalDeadlineComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldVerticalDeadlineComponent : MonoBehaviour
+{
+    SquadMovementComponent alturaDESTRUCT;
+    public float alturaMAX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alturaDESTRUCT = GetComponent<SquadMovementComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alturaDESTRUCT.Altura() <= alturaMAX) { Destroy(this.gameObject); }
+    }
+}
