b3cd6516328470a65a9f07785416dde39f840c0b
Javier Comas <82498887+javixxu@users.noreply.github.com>
2021-10-03 21:30:31 +0200

    xd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _isGameRunning;    
+    static private GameManager _instance;
+    static public GameManager Instance;
+    private GameObject[] _squads;
+    int Score=0;
+    float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if ((int)timer / 5 < 0) {
+            Debug.Log("Niggers");
+           
+        }
+        
+        Debug.Log(timer);
+    }
+    public void OnEnemyDies(int scoreToAdd) {
+       Score += scoreToAdd;    
+    }
+}
